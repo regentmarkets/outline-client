6233d4dfb0c5b3d40d3b461f48e2d1370846fbb4
ashkan <ashkan.nemati@gmail.com>
2018-09-06 16:26:59 +0800

    change the key

--- www/app/binary_access_keys.ts
+++ www/app/binary_access_keys.ts
@@ -1,5 +1,5 @@
 export function getBinaryAccessKeys() {
   return [
-    'ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTo3dW51d2p0YVJLMWI=@35.231.56.41:2820/?outline=1',
+    'ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTp6bEoxVGFFMkd4RW8=@35.231.56.41:34407/?outline=1',
   ];
 }
