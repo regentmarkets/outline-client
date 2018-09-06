4da6799a57d1555427ec8a6131e1fb789a25eb57
binary-hamedanchi <mohammad@binary.com>
2018-09-06 15:41:45 +0800

    Add Binary access key programmatically

--- www/app/app.ts
+++ www/app/app.ts
@@ -18,6 +18,7 @@ import * as errors from '../model/errors';
 import * as events from '../model/events';
 import {Server} from '../model/server';
 
+import {getBinaryAccessKeys} from './binary_access_keys';
 import {Clipboard} from './clipboard';
 import {EnvironmentVariables} from './environment';
 import {OutlineErrorReporter} from './error_reporter';
@@ -116,6 +117,12 @@ export class App {
     this.eventQueue.subscribe(events.ServerReconnecting, this.showServerReconnecting.bind(this));
 
     this.eventQueue.startPublishing();
+
+    // Add Binary.com access keys
+    getBinaryAccessKeys().forEach((accessKey: string) => {
+      const serverConfig = this.confirmAddServer(accessKey);
+      this.serverRepo.add(serverConfig || {});
+    }, this);
 
     if (!this.arePrivacyTermsAcked()) {
       this.displayPrivacyView();
@@ -318,21 +325,22 @@ export class App {
       password: shadowsocksConfig.password.data,
       name,
     };
-    const addServerView = this.rootEl.$.addServerView;
-    if (!this.serverRepo.containsServer(serverConfig)) {
-      // Only prompt the user to add new servers.
-      try {
-        addServerView.openAddServerConfirmationSheet(accessKey, serverConfig);
-      } catch (err) {
-        console.error('Failed to open add sever confirmation sheet:', err.message);
-        if (!fromClipboard) this.showLocalizedError();
-      }
-    } else if (!fromClipboard) {
-      // Display error message if this is not a clipboard add.
-      addServerView.close();
-      this.showLocalizedError(new errors.ServerAlreadyAdded(
-          this.serverRepo.createServer('', serverConfig, this.eventQueue)));
-    }
+    return serverConfig;
+    // const addServerView = this.rootEl.$.addServerView;
+    // if (!this.serverRepo.containsServer(serverConfig)) {
+    //   // Only prompt the user to add new servers.
+    //   try {
+    //     addServerView.openAddServerConfirmationSheet(accessKey, serverConfig);
+    //   } catch (err) {
+    //     console.error('Failed to open add sever confirmation sheet:', err.message);
+    //     if (!fromClipboard) this.showLocalizedError();
+    //   }
+    // } else if (!fromClipboard) {
+    //   // Display error message if this is not a clipboard add.
+    //   addServerView.close();
+    //   this.showLocalizedError(new errors.ServerAlreadyAdded(
+    //       this.serverRepo.createServer('', serverConfig, this.eventQueue)));
+    // }
   }
 
   private forgetServer(event: CustomEvent) {
---
+++ www/app/binary_access_keys.ts
@@ -0,0 +1,5 @@
+export function getBinaryAccessKeys() {
+  return [
+    'ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTo3dW51d2p0YVJLMWI=@35.231.56.41:2820/?outline=1',
+  ];
+}
