aa3ad1d019dfcb7f3609532696b06eb9764235fa
ashkan <ashkan.nemati@gmail.com>
2018-09-11 09:24:12 +0800

    add MacOS

--- apple/xcode/osx/OutlineLauncher/AppDelegate.swift
+++ apple/xcode/osx/OutlineLauncher/AppDelegate.swift
@@ -17,7 +17,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  static let kAppGroup = "QT8Z3Q9V3A.org.outline.macos.client"
+  static let kAppGroup = "QT8Z3Q9V3A. com.binary.outline.macos.client"
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
    defer {
